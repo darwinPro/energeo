ba15ca930e6f1671a0768f7b83244f4e0749c642
darwin <darwin.da64@gmail.com>
2022-06-23 00:04:45 -0500

    fix

--- front/src/utils/env.ts
+++ front/src/utils/env.ts
@@ -3,5 +3,5 @@ const env = process.env;
 export const port = parseInt(env.FRONT_PORT || "3000", 10);
 export const nodeEnv = env.NODE_ENV;
 export const dev = nodeEnv !== "production";
-export const backendUrl = env.BACK_URL;
-export const googleClientId = env.GOOGLE_CLIENT_ID;
+export const backendUrl = env.NEXT_PUBLIC_BACKEND_URL;
+export const googleClientId = env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
